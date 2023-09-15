766d876eef8fb55f341b1258da63039d41b4aadc
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-15 15:43:32 +0600

    Successfully loaded data in Courses component using the useEffect hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./components/Header/Header"
+import Courses from "./components/Courses/Courses"
+import Cart from "./components/Cart/Cart"
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <main className="container mx-auto grid grid-cols-[3fr_1fr]">
+        <Courses></Courses>
+        <Cart></Cart>
+      </main>
     </>
   )
 }
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 const Courses = () => {
+    const [courses, setCourses] = useState([])
+
+    useEffect(() => {
+        fetch('courses.json')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    }, [])
+
+    console.log("courses", courses);
     return (
         <div>
-            <h2>Courses</h2>
+            <h2>Courses: {courses.length}</h2>
         </div>
     );
 };
