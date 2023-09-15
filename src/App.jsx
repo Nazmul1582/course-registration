cc7f497333962351fc959ca1556a134f38686fdb
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-15 07:19:22 +0600

    Installed tailwindcss, configured template path and added the tailwind directives.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <h1>React Course Registration</h1>
+      <h1 className="text-5xl">React Course Registration</h1>
     </>
   )
 }
