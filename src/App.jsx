338d3c11f496769e72752421292d45f0bd45ca13
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-16 10:58:10 +0600

    showed toast as warning using daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,11 @@ function App() {
   const [creditRemaining, setCreditRemaining] = useState(20);
   const [totalCredit, setTotalCredit] = useState(0)
   const [totalPrice, setTotalPrice] = useState(0)
+  const [toast, setToast] = useState("")
 
   const handleSelectCourse = course => {
     if(selectedCourse.includes(course)){
-      alert(`${course.title} already selected!`)
+      setToast(`${course.title} already selected!`)
       return;
     }
     
@@ -25,13 +26,22 @@ function App() {
       const newSelectedCourse = [...selectedCourse, course]
       setSelectedCourse(newSelectedCourse);
     }else{
-      alert(`Your credit hour remaining only ${creditRemaining}`)
+      setToast(`Sorry! Your credit hour remaining only ${creditRemaining}`)
     }
   }
 
   return (
     <>
       <Header></Header>
+      {
+        toast && (
+          <div className="toast toast-top toast-end">
+            <div className="alert alert-error">
+              <span>{toast}</span>
+            </div>
+          </div>
+        )
+      }
       <main className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[2fr_1fr] xl:grid-cols-[3fr_1fr] gap-6">
         <Courses handleSelectCourse={handleSelectCourse}></Courses>
         <Cart 
